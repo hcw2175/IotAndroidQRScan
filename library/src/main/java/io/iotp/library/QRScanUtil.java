3f793d97c4692022db2fccc1063288e0e0f9495b
hucw <hcw2175@163.com>
2017-08-30 21:00:40 +0800

    初始化仓库文件

---
+++ library/src/main/java/io/iotp/library/QRScanUtil.java
@@ -0,0 +1,13 @@
+package io.iotp.library;
+
+/**
+ * QRScanUtil
+ *
+ * @author huchiwei
+ * @since 1.0.0
+ */
+public class QRScanUtil {
+    public static String test() {
+        return "QRScanUtil";
+    }
+}
